b7efde6546828fa85ea753b45862426147db0287
mehrik <sobreo.m@gmail.com>
2016-01-13 21:18:32 -0800

    Initial Commit

---
+++ twipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twipper
+//
+//  Created by Maric Sobreo on 1/13/16.
+//  Copyright © 2016 Maric Sobreo (Coding Dojo). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
